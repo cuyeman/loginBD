a954ad77246eb3f38a9068692242bc5ce43e6aef
cetecom <cetecom@LCV10811>
2019-04-15 22:46:53 -0300

    Creacion de Clases

--- clases/ley.php
+++ clases/ley.php
@@ -1,8 +1,69 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Ley{
+    private $idley;
+    private $numley;
+    private $fecpublicacion;
+    private $fecpromulgacion;
+    private $titulo;
+    private $fechahora;
+    
+    function __construct($idley, $numley, $fecpublicacion, $fecpromulgacion, $titulo, $fechahora) {
+        $this->idley = $idley;
+        $this->numley = $numley;
+        $this->fecpublicacion = $fecpublicacion;
+        $this->fecpromulgacion = $fecpromulgacion;
+        $this->titulo = $titulo;
+        $this->fechahora = $fechahora;
+    }
+    
+    function getIdley() {
+        return $this->idley;
+    }
+
+    function getNumley() {
+        return $this->numley;
+    }
+
+    function getFecpublicacion() {
+        return $this->fecpublicacion;
+    }
+
+    function getFecpromulgacion() {
+        return $this->fecpromulgacion;
+    }
+
+    function getTitulo() {
+        return $this->titulo;
+    }
+
+    function getFechahora() {
+        return $this->fechahora;
+    }
+
+    function setIdley($idley) {
+        $this->idley = $idley;
+    }
+
+    function setNumley($numley) {
+        $this->numley = $numley;
+    }
+
+    function setFecpublicacion($fecpublicacion) {
+        $this->fecpublicacion = $fecpublicacion;
+    }
+
+    function setFecpromulgacion($fecpromulgacion) {
+        $this->fecpromulgacion = $fecpromulgacion;
+    }
+
+    function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+
+    function setFechahora($fechahora) {
+        $this->fechahora = $fechahora;
+    }
+    
+}
 
